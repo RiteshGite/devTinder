e888e26c68eb0e60e63dbf99ad3f5f726eaa7a2d
Ritesh Gite <riteshgite2005@gmail.com>
2026-01-14 18:02:59 +0530

    created /user/connections API

--- src/router/user.js
+++ src/router/user.js
@@ -5,27 +5,70 @@ const { userAuth } = require("../middlewares/auth");
 const userRouter = express.Router();
 
 // get all the pending connection requests for the loggedInUser
-userRouter.get("/user/requests/received", userAuth, async (req, res) => {
-    const loggedInUserId = req.user?._id;
+userRouter.get("/user/requests/received", userAuth, async (req, res, next) => {
+    try {
+        const loggedInUserId = req.user?._id;
 
-    const connectionRequests = await ConnectionRequest.find({
-        toUserId: loggedInUserId,
-        status: "interested"
-    }).populate(
-        "fromUserId", 
-        "firstName lastName gender age photoUrl about skills"
-    );
+        const connectionRequests = await ConnectionRequest.find({
+            toUserId: loggedInUserId,
+            status: "interested"
+        }).populate(
+            "fromUserId",
+            "firstName lastName gender age photoUrl about skills"
+        );
 
-    if(!connectionRequests.length) {
-        return res.status(400).json({
-            success: false,
-            message: "No connection Requests"
+        if (!connectionRequests.length) {
+            return res.status(400).json({
+                success: false,
+                message: "No connection Requests"
+            })
+        }
+        res.status(200).json({
+            success: true,
+            requests: connectionRequests
         })
+    } catch (err) {
+        next(err);
     }
-    res.status(200).json({
-        success: true,
-        requests: connectionRequests
-    })
 });
+
+userRouter.get("/user/connections", userAuth, async (req, res, next) => {
+    try {
+        const loggedInUserId = req.user?._id;
+        
+        const USER_SAFE_DATA = "firstName lastName gender age photoUrl about skills"
+
+        const connections = await ConnectionRequest.find({
+            $or: [
+                {fromUserId: loggedInUserId},
+                {toUserId: loggedInUserId},
+            ],
+            status: "accepted"
+        })
+        .populate("fromUserId", USER_SAFE_DATA)
+        .populate("toUserId", USER_SAFE_DATA)
+
+        if(!connections.length) {
+            return res.status(200).json({
+                message: "No connections"
+            })
+        }
+
+        const data = connections.map(obj => {
+            if(obj.fromUserId._id.toString() === loggedInUserId.toString()) {
+                return obj.toUserId;
+            }
+            return obj.fromUserId;
+        })
+
+        res.status(200).json({
+            success: true,
+            totalConnections: connections.length,
+            connections: data
+        })
+    } catch (err) {
+        next(err);
+    }
+})
 
 module.exports = userRouter;
