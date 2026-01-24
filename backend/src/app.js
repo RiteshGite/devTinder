a681f336e4674ca3863d1f2aa1c59900c935eb92
Ritesh Gite <riteshgite2005@gmail.com>
2026-01-24 23:46:35 +0530

    backend all origin

--- backend/src/app.js
+++ backend/src/app.js
@@ -13,7 +13,7 @@ const cors = require("cors");
 const app = express();
 
 app.use(cors({
-    origin: process.env.CLIENT_ORIGIN,
+    origin: true,
     credentials: true,
 }));
 app.use(express.json());
