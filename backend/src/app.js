408aa629144e0fe835376b4a9cf6bcc9c2efa7c6
Ritesh Gite <riteshgite2005@gmail.com>
2026-01-25 00:32:03 +0530

    fix: enable cookie-based auth in production

--- backend/src/app.js
+++ backend/src/app.js
@@ -15,7 +15,7 @@ const cors = require("cors");
 const app = express();
 
 app.use(cors({
-    origin: true,
+    origin: "http://localhost:5173/",
     credentials: true,
 }));
 app.use(express.json());
